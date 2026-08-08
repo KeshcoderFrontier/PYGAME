cf918ee4eae4013d101154327ecf70dad547e639
KeshcoderFrontier <Thanujaniroshi85@gmail.com>
2026-08-08 13:11:38 +0100

    Add files via upload

---
+++ Let's top a window.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+root.title("Main")
+
+def topwin():
+    top = Toplevel()
+    top.geometry("180x100")
+    top.title("topLevel")
+
+
+
+    l2 = Label(top, text="This is a toplevel window")
+    l2.pack()
+
+    top.mainloop()
+
+
+l = Label(root, text="This is root window")
+btn = Button(root,text = "Click here to open another window", command=topwin)
+
+l.pack()
+btn.pack()
+
+
+root.mainloop
