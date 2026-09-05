71bd1d3fa71ad0247536c566a8eb3c6ab773ea2a
KeshcoderFrontier <Thanujaniroshi85@gmail.com>
2026-09-05 18:07:48 +0530

    Add files via upload

---
+++ NoteBookHandle2r.py
@@ -0,0 +1,42 @@
+import os
+
+print("=== Science Notes ===")
+with open("C:\\Users\\roshm\\OneDrive\\Desktop\\VS CODE STUFF\\.vscode\\Py game stuff\\Huge Projects\\Widgets\\Science-notes.txt","r") as f:
+    for line in f:
+        print(line.strip())
+print()
+
+
+print("===Word Count ===")
+with open("C:\\Users\\oshm\\OneDrive\\Desktop\\VS CODE STUFF\\.vscode\\Py game stuff\\Huge Projects\\Widgets\\maths-notes.txt", "r") as f:
+    for line in f:
+        words = line.split()
+        print(len(words),"words ->", line.strip())
+
+print()
+
+
+print("=== Merging Notes ===")
+if os.path.exists("all-notes.txt"):
+    print("all-notes.txt already exists - overwriting")
+else:
+    print("all-notes.txt not found - creating now")
+
+
+content = ""
+with open("C:\\Users\\roshm\\OneDrive\\Desktop\\VS CODE STUFF\\.vscode\\Py game stuff\\Huge Projects\\Widgets\\Science-notes.txt", "r") as f:
+    content += " --- science-notes.txt ---\n"
+    content += f.read() + "\n"
+with open("maths-notes.txt","r") as f:
+    content += " --- maths-notes.txt ---\n"
+    content += f.read() + "\n"
+with open("all-notes.txt", "w") as out:
+    out.write(content)
+print("Saved to all-notes .txt")
+print()
+
+if os.paths.exist("all-notes.txt"):
+    os.remove("all-notes.txt")
+    print("all-notes.txt deleted")
+else:
+    print("all-notes.txt does not exist")
